d46c4f33d1acb575673b84da29df4b1249c943d3
WasefUllah <wasefudvash@gmail.com>
2025-03-14 17:34:06 +0600

    fixed active buttons

--- script/index.js
+++ script/index.js
@@ -1,33 +1,70 @@
+// Remove active class
+const removeActiveClass = () => {
+  const activeButtons = document.getElementsByClassName("active");
+  for (const abtn of activeButtons) {
+    abtn.classList.remove("active");
+  }
+};
+
 // Load categories
 const loadCategories = () => {
   fetch(`https://openapi.programming-hero.com/api/phero-tube/categories`)
     .then((res) => res.json())
-    .then((data) => displayCategories(data.categories));
+    .then((data) => {
+      displayCategories(data.categories);
+    });
 };
 // Load videos
 const loadVideos = () => {
   fetch(`https://openapi.programming-hero.com/api/phero-tube/videos`)
     .then((res) => res.json())
-    .then((data) => displayVideos(data.videos));
+    .then((data) => {
+      removeActiveClass();
+      document.getElementById("allButton").classList.add("active");
+      displayVideos(data.videos);
+    });
 };
+// Load category videos
+const loadCategoryVideos = (id) => {
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const clickedButton = document.getElementById(`btn-${id}`);
+      // console.log(clickedButton);
+      removeActiveClass();
+      clickedButton.classList.add("active");
+      displayVideos(data.category);
+    });
+};
+
 // Display categories
 const displayCategories = (categories) => {
   const categoryContainer = document.getElementById("category-container");
   for (const cat of categories) {
     const categoryDiv = document.createElement("div");
     categoryDiv.innerHTML = `
-    <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+    <button id='btn-${cat.category_id}' onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
     `;
     categoryContainer.appendChild(categoryDiv);
   }
 };
 // Display video
 const displayVideos = (videos) => {
-    const videoContainer = document.getElementById("videoContainer");
-    for (const vid of videos) {
-        console.log(vid);
-        const videoCard = document.createElement("div");
-        videoCard.innerHTML=`
+  const videoContainer = document.getElementById("videoContainer");
+  videoContainer.innerHTML = ``;
+  if (videos.length == 0) {
+    videoContainer.innerHTML = `
+     <div class="col-span-full text-center flex flex-col justify-center items-center py-20">
+      <img class="w-[120px]" src="Icon.png" alt="">
+      <h2 class="text-2xl font-bold">Oops!! Sorry there is no content here.</h2>
+    </div>
+    `;
+    return;
+  }
+  for (const vid of videos) {
+    const videoCard = document.createElement("div");
+
+    videoCard.innerHTML = `
         <div class="card bg-base-100  shadow-sm">
     <figure class="relative">
       <img
@@ -45,15 +82,14 @@ const displayVideos = (videos) => {
         </div>
       </div>
       <div class="intro">
-        <h2 class="font-semibold text-sm">Hello world</h2>
+        <h2 class="font-semibold text-sm">${vid.title}</h2>
         <p class="text-sm text-gray-400 flex gap-2 justify-between items-center">${vid.authors[0].profile_name} <span><img class="w-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt=""></span></p> 
         <p class="text-sm text-gray-400">${vid.others.views} views</p>
       </div>
     </div>
   </div>
         `;
-        videoContainer.appendChild(videoCard);
-    }
-}
+    videoContainer.appendChild(videoCard);
+  }
+};
 loadCategories();
-loadVideos();
